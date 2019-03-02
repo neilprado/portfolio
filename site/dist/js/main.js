572fd958620d1d8c63fd60d78921d8ab073d9751
neilprado <neiljohn17@gmail.com>
2019-03-01 22:18:32 -0300

    Fixing eventListener

--- site/dist/js/main.js
+++ site/dist/js/main.js
@@ -2,11 +2,11 @@ const menuBtn = document.querySelector('.menu-btn')
 const menu = document.querySelector('.menu')
 const menuNav = document.querySelector('.menu-nav')
 const menuBranding = document.querySelector('.menu-branding')
-const navItems = document.querySelectorAll('.nav-item')
+const navItems = document.querySelectorAll('.nav-item') 
 
 let showMenu = false
 
-menuBtn.addEventListener('click', toggleMenu())
+menuBtn.addEventListener('click', toggleMenu)
 
 function toggleMenu(){
     if(!showMenu){
